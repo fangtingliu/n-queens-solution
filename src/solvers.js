2fe485d63ffe6a551501f6ea65fbf893170f9c1c
MKS-students <mks.students@hackreactor.com>
2015-08-05 20:08:23 -0500

    2nd attempt at nSolutions

--- src/solvers.js
+++ src/solvers.js
@@ -25,45 +25,107 @@ window.findNRooksSolution = function(n) {
 };
 
 
-
-// return the number of nxn chessboards that exist, with n rooks placed such that none of them can attack each other
 window.countNRooksSolutions = function(n) {
-  var solutionCount = 0;//fixme
-  var row = {};
-  var col = {};
-  var searchNextRow = function(row, col){
-    if (row.hasOwnProperty(n-1)) {
-      solutionCount ++;
-    } else if (!row.hasOwnProperty(0)) {
-      for (var colIndex = 0; colIndex < n; colIndex ++) {
-        var newRow = JSON.parse(JSON.stringify(row));
-        var newCol = JSON.parse(JSON.stringify(col));
-        newRow[0] = colIndex;
-        newCol[colIndex] = 0;
+  var count = 1;
+  var colObj = {};
+  var board = this.findNRooksSolution(n);
 
-        searchNextRow(newRow, newCol);
+  for ( var i = 0; i < n; i++ ) {
+    for ( var j = 0; j < n; j++ ) {
+      if ( board[i][j] === 1 ) {
+        colObj[j] = i;
       }
-    } else {
-      var newRow = JSON.parse(JSON.stringify(row));
-      var newCol = JSON.parse(JSON.stringify(col));
-      for (var rowIndex = n - 1; rowIndex >= 0; rowIndex --) {
-        if (newRow.hasOwnProperty(rowIndex)) {
-          for (var colIndex = 0; colIndex < n; colIndex ++) {
-            if (!newCol.hasOwnProperty(colIndex)) {
-              newRow[rowIndex + 1] = colIndex;
-              newCol[colIndex] = rowIndex + 1;
-              searchNextRow(newRow, newCol);
-            }
+    }
+  }
+
+  var search = function(board) {
+    for ( var row = n-1; row >= 0; row-- ) {
+      var pieceIndex = board[row].indexOf(1);
+      if ( pieceIndex < n-1 && pieceIndex > -1 ) {
+        board[row][pieceIndex] = 0;
+        delete colObj[pieceIndex];
+        for ( var nextIndex = pieceIndex + 1; nextIndex < n; nextIndex++ ) {
+          if ( !colObj.hasOwnProperty(nextIndex) ) {
+            board[row][nextIndex] = 1;
+            colObj[nextIndex] = row;
+            break;
+          }
+        }
+        return board;
+      } else if ( pieceIndex === n - 1 ) {
+        board[row][pieceIndex] = 0;
+        delete colObj[pieceIndex];
+      }
+    }
+    return null;
+  }
+
+  var build = function(board) {
+    if ( board === null ) {
+      return null;
+    }
+    for ( var row = 1; row < n; row++ ) {
+      if ( board[row].indexOf(1) < 0 ) {
+        for ( var col = 0; col < n; col++ ) {
+          if ( !colObj.hasOwnProperty(col) ) {
+            board[row][col] = 1;
+            colObj[col] = row;
+            break;
           }
         }
       }
     }
-  }
-  searchNextRow(row, col);
+    count++;
+    return board;
+  };
 
-  console.log('Number of solutions for ' + n + ' rooks:', solutionCount);
-  return solutionCount;
+  while ( true ) {
+    var result = build(search(board));
+    if ( result === null ) break;
+  }
+
+  return count;
 };
+
+// return the number of nxn chessboards that exist, with n rooks placed such that none of them can attack each other
+// window.countNRooksSolutions = function(n) {
+  // var solutionCount = 0;//fixme
+  // var row = {};
+  // var col = {};
+  // var searchNextRow = function(row, col){
+  //   if (row.hasOwnProperty(n-1)) {
+  //     solutionCount ++;
+  //   } else if (!row.hasOwnProperty(0)) {
+  //     for (var colIndexFirstRow = 0; colIndexFirstRow < n; colIndexFirstRow ++) {
+  //       var firstRow = JSON.parse(JSON.stringify(row));
+  //       var newColFirstRow = JSON.parse(JSON.stringify(col));
+  //       firstRow[0] = colIndexFirstRow;
+  //       newColFirstRow[colIndexFirstRow] = 0;
+
+  //       searchNextRow(firstRow, newColFirstRow);
+  //     }
+  //   } else {
+  //     var newRow = JSON.parse(JSON.stringify(row));
+  //     var newCol = JSON.parse(JSON.stringify(col));
+  //     for (var rowIndex = n - 1; rowIndex >= 0; rowIndex --) {
+  //       if (newRow.hasOwnProperty(rowIndex)) {
+  //         for (var colIndex = 0; colIndex < n; colIndex ++) {
+  //           if (!newCol.hasOwnProperty(colIndex)) {
+  //             newRow[rowIndex + 1] = colIndex;
+  //             newCol[colIndex] = rowIndex + 1;
+  //             searchNextRow(newRow, newCol);
+  //           }
+  //         }
+  //         break;
+  //       }
+  //     }
+  //   }
+  // }
+  // searchNextRow(row, col);
+
+  // console.log('Number of solutions for ' + n + ' rooks:', solutionCount);
+  // return solutionCount;
+// };
 
 
 
