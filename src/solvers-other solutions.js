7869bcf627f5875bdc2cf9d2a3ac047eebde68bb
Fangting <fangtingprahl@gmail.com>
2015-08-05 23:49:26 -0500

    Basic Requirement

---
+++ src/solvers-other solutions.js
@@ -0,0 +1,108 @@
+
+//using object instead of arrays significantly increased the run time.
+
+window.countNRooksSolutions = function(n) {
+  var solutionCount = 0;//fixme
+  var row = {};
+  var col = {};
+  var searchNextRow = function(row, col){
+    var newRow = JSON.parse(JSON.stringify(row));
+    var newCol = JSON.parse(JSON.stringify(col));
+
+    if (newRow.hasOwnProperty(n-1)) {
+      solutionCount ++;
+    } else if (!newRow.hasOwnProperty(0)) {
+      for (var colIndexFirstRow = 0; colIndexFirstRow < n; colIndexFirstRow ++) {
+        // var firstRow = JSON.parse(JSON.stringify(row));
+        // var newColFirstRow = JSON.parse(JSON.stringify(col));
+        var firstRow = {};
+        var newColFirstRow = {};
+        firstRow[0] = colIndexFirstRow;
+        newColFirstRow[colIndexFirstRow] = 0;
+        searchNextRow(firstRow, newColFirstRow);
+      }
+    } else {
+      for (var rowIndex = n - 1; rowIndex >= 0; rowIndex --) {
+        if (newRow.hasOwnProperty(rowIndex)) {
+          for (var colIndex = 0; colIndex < n; colIndex ++) {
+            if (!newCol.hasOwnProperty(colIndex)) {
+              newRow[rowIndex + 1] = colIndex;
+              newCol[colIndex] = rowIndex + 1;
+              searchNextRow(newRow, newCol);
+              delete newRow[rowIndex + 1];
+              delete newCol[colIndex];
+            }
+          }
+          break;
+        }
+      }
+    }
+  }
+  searchNextRow(row, col);
+
+  console.log('Number of solutions for ' + n + ' rooks:', solutionCount);
+  return solutionCount;
+};
+
+
+window.countNRooksSolutions = function(n) {
+  var count = 1;
+  var colObj = {};
+  var board = this.findNRooksSolution(n);
+
+  for ( var i = 0; i < n; i++ ) {
+    for ( var j = 0; j < n; j++ ) {
+      if ( board[i][j] === 1 ) {
+        colObj[j] = i;
+      }
+    }
+  }
+
+  var search = function(board) {
+    for ( var row = n-1; row >= 0; row-- ) {
+      var pieceIndex = board[row].indexOf(1);
+      if ( pieceIndex < n-1 && pieceIndex > -1 ) {
+        board[row][pieceIndex] = 0;
+        delete colObj[pieceIndex];
+        for ( var nextIndex = pieceIndex + 1; nextIndex < n; nextIndex++ ) {
+          if ( !colObj.hasOwnProperty(nextIndex) ) {
+            board[row][nextIndex] = 1;
+            colObj[nextIndex] = row;
+            break;
+          }
+        }
+        return board;
+      } else if ( pieceIndex === n - 1 ) {
+        board[row][pieceIndex] = 0;
+        delete colObj[pieceIndex];
+      }
+    }
+    return null;
+  }
+
+  var build = function(board) {
+    if ( board === null ) {
+      return null;
+    }
+    for ( var row = 1; row < n; row++ ) {
+      if ( board[row].indexOf(1) < 0 ) {
+        for ( var col = 0; col < n; col++ ) {
+          if ( !colObj.hasOwnProperty(col) ) {
+            board[row][col] = 1;
+            colObj[col] = row;
+            break;
+          }
+        }
+      }
+    }
+    count++;
+    return board;
+  };
+
+  while ( true ) {
+    var result = build(search(board));
+    if ( result === null ) break;
+  }
+
+  return count;
+};
--- src/solvers.js
+++ src/solvers.js
@@ -25,113 +25,114 @@ window.findNRooksSolution = function(n) {
 };
 
 
+// return the number of nxn chessboards that exist, with n rooks placed such that none of them can attack each other
 window.countNRooksSolutions = function(n) {
-  var count = 1;
-  var colObj = {};
-  var board = this.findNRooksSolution(n);
-
-  for ( var i = 0; i < n; i++ ) {
-    for ( var j = 0; j < n; j++ ) {
-      if ( board[i][j] === 1 ) {
-        colObj[j] = i;
+  var solutionCount = 0;//fixme
+  var solution = [];
+  var lastHead = false;
+  var searchNextRow = function(temp){
+    var solution = temp.slice();
+    if (solution.length === n) {
+      solutionCount ++;
+      // the following 2 lines of code are used for efficiency only; decrease running time about 50%
+      if ((n > 1) && (!lastHead)) {
+        solutionCount ++;
+      }      
+    } else if (solution.length === 0) {
+      for (var colIndexFirstRow = 0; colIndexFirstRow < Math.ceil(n/2); colIndexFirstRow ++) {
+        solution[0] = [];
+        solution[0][colIndexFirstRow] = 1;
+        // the following 1 lines of code are used for efficiency only; decrease running time about 50%
+        if (!(n % 2 === 0) && (colIndexFirstRow === Math.ceil(n/2) -1)) lastHead = true;
+        searchNextRow(solution);
+        solution.pop();
       }
-    }
-  }
-
-  var search = function(board) {
-    for ( var row = n-1; row >= 0; row-- ) {
-      var pieceIndex = board[row].indexOf(1);
-      if ( pieceIndex < n-1 && pieceIndex > -1 ) {
-        board[row][pieceIndex] = 0;
-        delete colObj[pieceIndex];
-        for ( var nextIndex = pieceIndex + 1; nextIndex < n; nextIndex++ ) {
-          if ( !colObj.hasOwnProperty(nextIndex) ) {
-            board[row][nextIndex] = 1;
-            colObj[nextIndex] = row;
-            break;
+    } else {
+      for (var rowIndex = n - 1; rowIndex >= 0; rowIndex --) {
+        if (Array.isArray(solution[rowIndex])) {
+          for (var colIndex = 0; colIndex < n; colIndex ++) {
+            for (var currRow = 0; currRow <= rowIndex ; currRow ++) {
+              if (solution[currRow][colIndex] === 1) {
+                break;
+              }
+            }
+            if (currRow > rowIndex) {
+              solution[rowIndex + 1] = [];
+              solution[rowIndex + 1][colIndex] = 1;
+              searchNextRow(solution);
+              solution.pop();
+            }
           }
-        }
-        return board;
-      } else if ( pieceIndex === n - 1 ) {
-        board[row][pieceIndex] = 0;
-        delete colObj[pieceIndex];
-      }
-    }
-    return null;
-  }
-
-  var build = function(board) {
-    if ( board === null ) {
-      return null;
-    }
-    for ( var row = 1; row < n; row++ ) {
-      if ( board[row].indexOf(1) < 0 ) {
-        for ( var col = 0; col < n; col++ ) {
-          if ( !colObj.hasOwnProperty(col) ) {
-            board[row][col] = 1;
-            colObj[col] = row;
-            break;
-          }
+          break;
         }
       }
     }
-    count++;
-    return board;
-  };
-
-  while ( true ) {
-    var result = build(search(board));
-    if ( result === null ) break;
   }
+  searchNextRow(solution);
 
-  return count;
+  console.log('Number of solutions for ' + n + ' rooks:', solutionCount);
+  return solutionCount;
 };
 
-// return the number of nxn chessboards that exist, with n rooks placed such that none of them can attack each other
-// window.countNRooksSolutions = function(n) {
-  // var solutionCount = 0;//fixme
-  // var row = {};
-  // var col = {};
-  // var searchNextRow = function(row, col){
-  //   if (row.hasOwnProperty(n-1)) {
-  //     solutionCount ++;
-  //   } else if (!row.hasOwnProperty(0)) {
-  //     for (var colIndexFirstRow = 0; colIndexFirstRow < n; colIndexFirstRow ++) {
-  //       var firstRow = JSON.parse(JSON.stringify(row));
-  //       var newColFirstRow = JSON.parse(JSON.stringify(col));
-  //       firstRow[0] = colIndexFirstRow;
-  //       newColFirstRow[colIndexFirstRow] = 0;
-
-  //       searchNextRow(firstRow, newColFirstRow);
-  //     }
-  //   } else {
-  //     var newRow = JSON.parse(JSON.stringify(row));
-  //     var newCol = JSON.parse(JSON.stringify(col));
-  //     for (var rowIndex = n - 1; rowIndex >= 0; rowIndex --) {
-  //       if (newRow.hasOwnProperty(rowIndex)) {
-  //         for (var colIndex = 0; colIndex < n; colIndex ++) {
-  //           if (!newCol.hasOwnProperty(colIndex)) {
-  //             newRow[rowIndex + 1] = colIndex;
-  //             newCol[colIndex] = rowIndex + 1;
-  //             searchNextRow(newRow, newCol);
-  //           }
-  //         }
-  //         break;
-  //       }
-  //     }
-  //   }
-  // }
-  // searchNextRow(row, col);
-
-  // console.log('Number of solutions for ' + n + ' rooks:', solutionCount);
-  // return solutionCount;
-// };
 
 
 
 // return a matrix (an array of arrays) representing a single nxn chessboard, with n queens placed such that none of them can attack each other
 window.findNQueensSolution = function(n) {
-  var solution = undefined; //fixme
+  var solution = [];
+  var complete = false;
+  var rowObj;
+  var searchNextRow = function(temp){
+    if (complete) return;
+    solution = temp.slice();
+    if (solution.length === n) {
+      complete = true;
+      return;
+    } else if (solution.length === 0) {
+      for (var colIndexFirstRow = 0; colIndexFirstRow < n; colIndexFirstRow ++) {
+        solution[0] = [];
+        solution[0][colIndexFirstRow] = 1;
+        rowObj = {0: colIndexFirstRow};
+        searchNextRow(solution);
+        if (complete) return;
+        solution.pop();
+      }
+    } else {
+      for (var rowIndex = n - 1; rowIndex >= 0; rowIndex --) {
+        if (Array.isArray(solution[rowIndex])) {
+          for (var colIndex = 0; colIndex < n; colIndex ++) {
+            var valid = true;
+            for (var currRow = 0; currRow <= rowIndex ; currRow ++) {
+              if (solution[currRow][colIndex] === 1) {
+                valid = false;
+                break;
+              } else {
+                for (var key in rowObj) {
+                  if (Math.abs((rowIndex + 1 - Number(key))/(colIndex - rowObj[key])) === 1){
+                    var valid = false;
+                    break;
+                  }
+                }
+              }
+            }
+            if (valid) {
+              solution[rowIndex + 1] = [];
+              solution[rowIndex + 1][colIndex] = 1;
+              rowObj[rowIndex+1] = colIndex;
+              searchNextRow(solution);
+              if (complete) return;
+              solution.pop();
+              delete rowObj[rowIndex+1];
+            }
+          }
+          break;
+        }
+      }
+    }
+  }
+  searchNextRow(solution);
+
+  !complete && (solution = new Array(n));
 
   console.log('Single solution for ' + n + ' queens:', JSON.stringify(solution));
   return solution;
@@ -140,11 +141,64 @@ window.findNQueensSolution = function(n) {
 
 // return the number of nxn chessboards that exist, with n queens placed such that none of them can attack each other
 window.countNQueensSolutions = function(n) {
-  var solutionCount = undefined; //fixme
+  var solutionCount = 0;
+  var rowObj = {0:0};
+  var searchNextRow = function(temp){
+    var solution = temp ? temp.slice() : [];
+    if (solution.length === n) {
+      solutionCount ++;
+      // the following few lines of code are used for efficiency only; decrease running time about 50%
+      var lastHead = n % 2 === 0 ? n/2 : (Math.ceil(n/2) - 1); 
+      if ((n > 1) && (rowObj[0] !== lastHead)) {
+        solutionCount ++;
+      }
+    } else if (solution.length === 0) {
+      for (var colIndexFirstRow = 0; colIndexFirstRow < Math.ceil(n/2); colIndexFirstRow ++) {
+        solution[0] = [];
+        solution[0][colIndexFirstRow] = 1;
+        rowObj = {0: colIndexFirstRow};
+        searchNextRow(solution);
+        solution.pop();
+      }
+    } else {
+      for (var rowIndex = n - 1; rowIndex >= 0; rowIndex --) {
+        if (Array.isArray(solution[rowIndex])) {
+          for (var colIndex = 0; colIndex < n; colIndex ++) {
+            var valid = true;
+            for (var currRow = 0; currRow <= rowIndex ; currRow ++) {
+              if (solution[currRow][colIndex] === 1) {
+                valid = false;
+                break;
+              } else {
+                for (var key in rowObj) {
+                  if (Math.abs((rowIndex + 1 - Number(key))/(colIndex - rowObj[key])) === 1){
+                    var valid = false;
+                    break;
+                  }
+                }
+              }
+            }
+            if (valid) {
+              solution[rowIndex + 1] = [];
+              solution[rowIndex + 1][colIndex] = 1;
+              rowObj[rowIndex+1] = colIndex;
+              searchNextRow(solution);
+              solution.pop();
+              delete rowObj[rowIndex+1];
+            }
+          }
+          break;
+        }
+      }
+    }
+  }
+  searchNextRow();
 
   console.log('Number of solutions for ' + n + ' queens:', solutionCount);
   return solutionCount;
 };
+
+
 
 window.countNQueensBitwise = function(n){
   var solutionCount = 0;
